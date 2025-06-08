08380ab79b1c0d043e643091f022e79786f0b21a
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2025-06-08 12:06:58 +0300

    Backup config

--- scripts/cfg-commit.nix
+++ scripts/cfg-commit.nix
@@ -11,21 +11,21 @@ pkgs.writeShellApplication {
     pushd /etc/nixos
 
     printf "\n=================\n"
-    echo     "Formatting files:"
-    printf "\n=================\n"
+    printf "Formatting files:\n"
+    printf "=================\n\n"
     ${pkgs.nixfmt-tree}/bin/treefmt
 
     ${pkgs.git}/bin/git add -A
 
     printf "\n=====================\n"
-    echo     "Running 'git commit':"
-    printf "\n=====================\n"
+    printf "Running 'git commit':\n"
+    printf "=====================\n\n"
     ${pkgs.git}/bin/git commit -m "Backup config"
 
 
     printf "\n===================\n"
-    echo     "Running 'git push':"
-    printf "\n===================\n"
+    printf "Running 'git push':\n"
+    printf "===================\n\n"
     ${pkgs.git}/bin/git push
 
     printf
