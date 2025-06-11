0069df82d6715169732db63fac04fa9a3e25dce5
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2025-06-11 13:13:23 +0300

    Backup config

--- vpn.nix
+++ vpn.nix
@@ -1,10 +1,6 @@
 { ... }:
 {
-  environment.shellAliases = {
-    vpn-up = "sudo systemctl start openvpn-omp";
-    vpn-down = "sudo systemctl stop openvpn-omp";
-  };
-
+  # sudo systemctl start/stop openvpn-omp
   services.openvpn.servers = {
     omp = {
       config = "config /root/n.krasnov.ovpn";
