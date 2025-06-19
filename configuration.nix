69e19988c15485d068456d0f2bf225ce24a5c2f4
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2025-06-19 19:17:29 +0300

    Backup config

--- configuration.nix
+++ configuration.nix
@@ -57,66 +57,10 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
-  services.xserver.enable = true;
-
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  services.xserver.excludePackages = [ pkgs.xterm ];
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-tour
-    epiphany
-    gnome-connections
-    yelp
-    gnome-maps
-    gnome-music
-    decibels
-    totem
-    geary
-  ];
-
-  services.xserver.xkb = {
-    layout = "us,ru";
-    variant = "";
-    options = "caps:escape";
-  };
-
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  services.libinput.enable = true;
-
-  users.users.synalice = {
-    isNormalUser = true;
-    description = "synalice";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-  };
-
   nixpkgs.config.allowUnfree = true;
 
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
-  };
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
-      AllowUsers = [ "synalice" ];
-    };
   };
 
   system.stateVersion = "25.05";
--- services.nix
+++ services.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.acpid.enable = true;
 
@@ -10,6 +10,62 @@
     path = [ "/run/current-system/sw" ];
     serviceConfig = {
       Type = "oneshot";
+    };
+  };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    epiphany
+    gnome-connections
+    yelp
+    gnome-maps
+    gnome-music
+    decibels
+    totem
+    geary
+  ];
+
+  services.xserver.xkb = {
+    layout = "us,ru";
+    variant = "";
+    options = "caps:escape";
+  };
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.libinput.enable = true;
+
+  users.users.synalice = {
+    isNormalUser = true;
+    description = "synalice";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "synalice" ];
     };
   };
 }
