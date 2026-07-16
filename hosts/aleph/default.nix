4a210ba09f00b2fb2afbb69136f535ebe5d3b755
Nika Krasnova <nika@nikableh.moe>
2026-07-17 00:07:51 +0200

    Enable Windows 11 boot entry on aleph

--- hosts/aleph/default.nix
+++ hosts/aleph/default.nix
@@ -4,14 +4,12 @@
   networking.hostName = "aleph";
 
   # Chainload Windows from its own ESP (nvme0n1p1, FS UUID 7CC4-56C8).
-  # To find the device handle: reboot, pick "EDK2 UEFI Shell" from the boot
-  # menu, run `map -c`, then `ls <handle>:\EFI` until one contains `Microsoft`.
   boot.loader.systemd-boot = {
     edk2-uefi-shell.enable = true;
-    # windows."11" = {
-    #   title = "Windows 11";
-    #   efiDeviceHandle = "HD0b"; # <- replace with the discovered handle
-    # };
+    windows."11" = {
+      title = "Windows 11";
+      efiDeviceHandle = "HD0b";
+    };
   };
 
   # Don't allow the Wi-Fi adapter (8086:272b) to enter D3cold.
