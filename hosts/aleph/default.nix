c9aeaf8310bafc1c1145584b7fda1f2e987b4253
Nika Krasnova <nika@nikableh.moe>
2026-07-19 13:35:33 +0200

    Move Windows 11 boot entry to hardware-configuration

--- hosts/aleph/default.nix
+++ hosts/aleph/default.nix
@@ -3,19 +3,5 @@
 
   networking.hostName = "aleph";
 
-  # Chainload Windows from its own ESP (nvme0n1p1, FS UUID 7CC4-56C8).
-  boot.loader.systemd-boot = {
-    edk2-uefi-shell.enable = true;
-    windows."11" = {
-      title = "Windows 11";
-      efiDeviceHandle = "HD0b";
-    };
-  };
-
-  # Don't allow the Wi-Fi adapter (8086:272b) to enter D3cold.
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x272b", ATTR{d3cold_allowed}="0"
-  '';
-
   system.stateVersion = "26.05";
 }
--- hosts/aleph/hardware-configuration.nix
+++ hosts/aleph/hardware-configuration.nix
@@ -1,29 +1,60 @@
-{ config, lib, modulesPath, ... }:
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "thunderbolt"
+      "nvme"
+      "usb_storage"
+      "sd_mod"
+      "sdhci_pci"
     ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2b611c7e-5636-4bc4-ad2a-18ef8135dde8";
-      fsType = "ext4";
+    # Chainload Windows from its own ESP (nvme0n1p1, FS UUID 7CC4-56C8).
+    loader.systemd-boot = {
+      edk2-uefi-shell.enable = true;
+      windows."11" = {
+        title = "Windows 11";
+        efiDeviceHandle = "HD0b";
+      };
     };
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/79DD-D7DE";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/2b611c7e-5636-4bc4-ad2a-18ef8135dde8";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/79DD-D7DE";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
 
   swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.npu.enable = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Don't allow the Wi-Fi adapter (8086:272b) to enter D3cold.
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x272b", ATTR{d3cold_allowed}="0"
+  '';
 }
