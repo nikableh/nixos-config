71d6e85c5382ab7393b63046bb6e03621191a637
Nika Krasnova <nika@nikableh.moe>
2026-08-08 17:07:02 +0200

    Delete cfg-commit script

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -2,7 +2,6 @@
 {
   home.packages = [
     (pkgs.callPackage ./cfg-cd.nix { })
-    (pkgs.callPackage ./cfg-commit.nix { })
     (pkgs.callPackage ./cfg-open.nix { })
     (pkgs.callPackage ./cfg-rebuild.nix { })
     (pkgs.callPackage ./git-oss.nix { })
